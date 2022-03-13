e8751cf4a917b39d3ac0f87d70e36c542d9d1b03
Jorge Cisneros <cisneros.jorge.a@gmail.com>
2022-03-12 20:18:06 -0600

    added url creation

--- weather.go
+++ weather.go
@@ -9,29 +9,41 @@ import (
 
 // Make API call, response is returned
 func getRequest(url string) *http.Response {
+
 	// Create client
 	client := &http.Client{}
+
 	// Build request
-	req, err := http.NewRequest("GET", url, nil)
+	request, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		log.Println(err)
 	}
+
 	// Add headers to request
 	req.Header.Add("User-Agent", "github.com/rBigChill")
-	// Make Request
-	resp, err := client.Do(req)
+
+	// Make Request, Get Response
+	response, err := client.Do(request)
 	if err != nil {
 		log.Println(err)
 	}
-	return resp
+	return response
 }
 
-func main() {
+func makeURL(endpoint string) string {
+
 	// Create url
 	url := url.URL{
 		Scheme: "https",
 		Host:   "api.weather.gov",
 	}
-	resp := getRequest(url.String())
-	fmt.Println(resp.Status)
+	response := getRequest(url.String())
+	return response
+}
+
+func main() {
+
+	endpoint := "points"
+	response := makeURL(endpoint)
+	fmt.Println(response.Status)
 }
